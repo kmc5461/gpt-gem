bc4c496526222c3b18f52f171205a95e2575a944
kmc5461 <kamaciacente@gmail.com>
2025-11-26 16:43:38 +0300

    Update route.ts

--- app/api/payments/stripe/route.ts
+++ app/api/payments/stripe/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/payments/stripe';
 import { prisma } from '@/lib/prisma'; // Projenizdeki prisma client
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function POST(req: Request) {
   try {
